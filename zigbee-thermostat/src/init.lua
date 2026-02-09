118d8377133a2fb6efad502f47b266a1f1a15fdc
Andrew Me <veonua@users.noreply.github.com>
2026-02-09 15:24:15 +0100

    support W500 floor thermostat

---
+++ zigbee-thermostat/src/init.lua
@@ -0,0 +1,361 @@
+-- Zigbee Driver utilities
+local ZigbeeDriver          = require "st.zigbee"
+local device_management     = require "st.zigbee.device_management"
+local defaults              = require "st.zigbee.defaults"
+local utils                 = require "st.utils"
+local constants             = require "st.zigbee.constants"
+local xiaomi_utils          = require "xiaomi_utils"
+
+-- Zigbee Spec Utils
+local clusters                      = require "st.zigbee.zcl.clusters"
+local PowerConfiguration            = clusters.PowerConfiguration
+local Thermostat                    = clusters.Thermostat
+local TemperatureMeasurement        = clusters.TemperatureMeasurement
+local ElectricalMeasurement          = clusters.ElectricalMeasurement
+local SimpleMetering                 = clusters.SimpleMetering
+
+local ThermostatSystemMode      = Thermostat.attributes.SystemMode
+local ThermostatControlSequence = Thermostat.attributes.ControlSequenceOfOperation
+
+-- Capabilities
+local capabilities              = require "st.capabilities"
+local Temperature               = capabilities.temperatureMeasurement
+local ThermostatCoolingSetpoint = capabilities.thermostatCoolingSetpoint
+local ThermostatHeatingSetpoint = capabilities.thermostatHeatingSetpoint
+local ThermostatMode            = capabilities.thermostatMode
+local ThermostatOperatingState  = capabilities.thermostatOperatingState
+local Battery                   = capabilities.battery
+local PowerSource               = capabilities.powerSource
+local RelativeHumidity          = capabilities.relativeHumidityMeasurement
+local PowerMeter                = capabilities.powerMeter
+local EnergyMeter               = capabilities.energyMeter
+local Refresh                   = capabilities.refresh
+
+-- lux thermostat uses min 5V, max of 6.5V
+local BAT_MIN = 50.0
+local BAT_MAX = 65.0
+
+local W500_MODEL = "lumi.airrtc.aeu001"
+
+local THERMOSTAT_MODE_MAP = {
+  [ThermostatSystemMode.OFF]               = ThermostatMode.thermostatMode.off,
+  [ThermostatSystemMode.AUTO]              = ThermostatMode.thermostatMode.auto,
+  [ThermostatSystemMode.COOL]              = ThermostatMode.thermostatMode.cool,
+  [ThermostatSystemMode.HEAT]              = ThermostatMode.thermostatMode.heat,
+  [ThermostatSystemMode.EMERGENCY_HEATING] = ThermostatMode.thermostatMode.emergency_heat
+}
+
+-- Map the Zigbee attribute value to the corresponding capability for supported modes
+local SUPPORTED_THERMOSTAT_MODES = {
+  [ThermostatControlSequence.COOLING_ONLY]                    = { ThermostatMode.thermostatMode.off.NAME,
+                                                                  ThermostatMode.thermostatMode.cool.NAME},
+  [ThermostatControlSequence.COOLING_WITH_REHEAT]             = { ThermostatMode.thermostatMode.off.NAME,
+                                                                  ThermostatMode.thermostatMode.cool.NAME},
+  [ThermostatControlSequence.HEATING_ONLY]                    = { ThermostatMode.thermostatMode.off.NAME,
+                                                                  ThermostatMode.thermostatMode.heat.NAME,
+                                                                  ThermostatMode.thermostatMode.emergency_heat.NAME},
+  [ThermostatControlSequence.HEATING_WITH_REHEAT]             = { ThermostatMode.thermostatMode.off.NAME,
+                                                                  ThermostatMode.thermostatMode.heat.NAME,
+                                                                  ThermostatMode.thermostatMode.emergency_heat.NAME},
+  [ThermostatControlSequence.COOLING_AND_HEATING4PIPES]             = { ThermostatMode.thermostatMode.off.NAME,
+                                                                        ThermostatMode.thermostatMode.heat.NAME,
+                                                                        ThermostatMode.thermostatMode.auto.NAME,
+                                                                        ThermostatMode.thermostatMode.cool.NAME,
+                                                                        ThermostatMode.thermostatMode.emergency_heat.NAME},
+  [ThermostatControlSequence.COOLING_AND_HEATING4PIPES_WITH_REHEAT] = { ThermostatMode.thermostatMode.off.NAME,
+                                                                        ThermostatMode.thermostatMode.heat.NAME,
+                                                                        ThermostatMode.thermostatMode.auto.NAME,
+                                                                        ThermostatMode.thermostatMode.cool.NAME,
+                                                                        ThermostatMode.thermostatMode.emergency_heat.NAME}
+}
+
+local W500_SUPPORTED_MODES = {
+  ThermostatMode.thermostatMode.off.NAME,
+  ThermostatMode.thermostatMode.heat.NAME
+}
+
+-- TemperatureMeasurement cluster defaults
+local temperature_measurement_defaults = {
+  MIN_TEMP = "MIN_TEMP",
+  MAX_TEMP = "MAX_TEMP"
+}
+
+local battery_voltage_handler = function(driver, device, battery_voltage)
+  if (battery_voltage.value == 0) then -- this means we're plugged in
+    device:emit_event(PowerSource.powerSource.mains())
+    device:emit_event(Battery.battery(100))
+  else
+    local perc_value = utils.round((battery_voltage.value - BAT_MIN)/(BAT_MAX - BAT_MIN) * 100)
+    device:emit_event(Battery.battery(utils.clamp_value(perc_value, 0, 100)))
+  end
+end
+
+local function is_w500(device)
+  return device:get_model() == W500_MODEL
+end
+
+local function get_divisor(device, key)
+  local divisor = device:get_field(key)
+  if divisor == nil or divisor == 0 then
+    return 1
+  end
+  return divisor
+end
+
+local function active_power_handler(driver, device, value, zb_rx)
+  local divisor = get_divisor(device, constants.ELECTRICAL_MEASUREMENT_DIVISOR_KEY)
+  local power = value.value / divisor
+  device:emit_event(PowerMeter.power({ value = power, unit = "W" }))
+end
+
+local function current_summation_delivered_handler(driver, device, value, zb_rx)
+  local divisor = get_divisor(device, constants.SIMPLE_METERING_DIVISOR_KEY)
+  local energy = value.value / divisor
+  device:emit_event(EnergyMeter.energy({ value = energy, unit = "kWh" }))
+end
+
+local power_source_handler = function(driver, device, battery_alarm_mask)
+  if (battery_alarm_mask:is_bit_set(31)) then
+    device:emit_event(PowerSource.powerSource.battery())
+  else
+    device:emit_event(PowerSource.powerSource.mains())
+  end
+end
+
+local supported_thermostat_modes_handler = function(driver, device, supported_modes)
+  if is_w500(device) then
+    device:emit_event(ThermostatMode.supportedThermostatModes(W500_SUPPORTED_MODES, { visibility = { displayed = false } }))
+    return
+  end
+
+  device:emit_event(ThermostatMode.supportedThermostatModes(SUPPORTED_THERMOSTAT_MODES[supported_modes.value], { visibility = { displayed = false } }))
+end
+
+local thermostat_mode_handler = function(driver, device, thermostat_mode)
+  if THERMOSTAT_MODE_MAP[thermostat_mode.value] then
+    device:emit_event(THERMOSTAT_MODE_MAP[thermostat_mode.value]())
+  end
+end
+
+local thermostat_operating_state_handler = function(driver, device, operating_state)
+  if (operating_state:is_heat_second_stage_on_set() or operating_state:is_heat_on_set()) then
+    device:emit_event(ThermostatOperatingState.thermostatOperatingState.heating())
+  elseif (operating_state:is_cool_second_stage_on_set() or operating_state:is_cool_on_set()) then
+    device:emit_event(ThermostatOperatingState.thermostatOperatingState.cooling())
+  elseif (operating_state:is_fan_on_set()) then
+    device:emit_event(ThermostatOperatingState.thermostatOperatingState.fan_only())
+  else
+    device:emit_event(ThermostatOperatingState.thermostatOperatingState.idle())
+  end
+end
+
+local set_thermostat_mode = function(driver, device, command)
+  for zigbee_attr_val, st_cap_val in pairs(THERMOSTAT_MODE_MAP) do
+    if command.args.mode == st_cap_val.NAME then
+      device:send_to_component(command.component, Thermostat.attributes.SystemMode:write(device, zigbee_attr_val))
+      device.thread:call_with_delay(1, function(d)
+        device:send_to_component(command.component, Thermostat.attributes.SystemMode:read(device))
+      end)
+      break
+    end
+  end
+end
+
+local thermostat_mode_setter = function(mode_name)
+  return function(driver, device, command)
+    return set_thermostat_mode(driver, device, {component = command.component, args = {mode = mode_name}})
+  end
+end
+
+local setpoint_limit_handler_factory = function(min_or_max, heat_or_cool)
+
+  local field = 'setpoint_' .. min_or_max .. '_' .. heat_or_cool
+  local paired_field = 'setpoint_min_' .. heat_or_cool
+  if min_or_max == 'min' then
+    paired_field = 'setpoint_max_' .. heat_or_cool
+  end
+
+  return function(driver, device, setpoint)
+    local celsius_value =  setpoint.value / 100.0
+    device:set_field(field, celsius_value)
+    if device:get_field(field) and device:get_field(paired_field) then
+
+      local event_constructor = capabilities.thermostatHeatingSetpoint.heatingSetpointRange
+      if heat_or_cool == 'cool' then
+        event_constructor = capabilities.thermostatCoolingSetpoint.coolingSetpointRange
+      end
+
+      device:emit_event(event_constructor(
+        {
+          unit = 'C',
+          value = {
+            minimum = device:get_field('setpoint_min_' .. heat_or_cool),
+            maximum = device:get_field('setpoint_max_' .. heat_or_cool)
+          }
+        }
+      ))
+
+      device:set_field(field, nil)
+      device:set_field(paired_field, nil)
+    end
+  end
+end
+
+--TODO: Update this once we've decided how to handle setpoint commands
+local set_setpoint_factory = function(setpoint_attribute)
+  return function(driver, device, command)
+    local value = command.args.setpoint
+    if (value >= 40) then -- assume this is a fahrenheit value
+      value = utils.f_to_c(value)
+    end
+    device:send_to_component(command.component, setpoint_attribute:write(device, utils.round(value*100)))
+
+    device.thread:call_with_delay(2, function(d)
+      device:send_to_component(command.component, setpoint_attribute:read(device))
+    end)
+  end
+end
+
+local temperature_measurement_min_max_attr_handler = function(minOrMax)
+  return function(driver, device, value, zb_rx)
+    local raw_temp = value.value
+    local celc_temp = raw_temp / 100.0
+    local temp_scale = "C"
+
+    device:set_field(string.format("%s", minOrMax), celc_temp)
+
+    local min = device:get_field(temperature_measurement_defaults.MIN_TEMP)
+    local max = device:get_field(temperature_measurement_defaults.MAX_TEMP)
+
+    if min ~= nil and max ~= nil then
+      if min < max then
+        device:emit_event_for_endpoint(zb_rx.address_header.src_endpoint.value, capabilities.temperatureMeasurement.temperatureRange({ value = { minimum = min, maximum = max }, unit = temp_scale }))
+        device:set_field(temperature_measurement_defaults.MIN_TEMP, nil)
+        device:set_field(temperature_measurement_defaults.MAX_TEMP, nil)
+      else
+        device.log.warn_with({hub_logs = true}, string.format("Device reported a min temperature %d that is not lower than the reported max temperature %d", min, max))
+      end
+    end
+  end
+end
+
+local do_refresh = function(self, device)
+  local attributes = {
+    Thermostat.attributes.OccupiedCoolingSetpoint,
+    Thermostat.attributes.OccupiedHeatingSetpoint,
+    Thermostat.attributes.LocalTemperature,
+    Thermostat.attributes.ControlSequenceOfOperation,
+    Thermostat.attributes.ThermostatRunningState,
+    Thermostat.attributes.SystemMode,
+    Thermostat.attributes.MinHeatSetpointLimit,
+    Thermostat.attributes.MaxHeatSetpointLimit,
+    Thermostat.attributes.MinCoolSetpointLimit,
+    Thermostat.attributes.MaxCoolSetpointLimit,
+  }
+
+  if is_w500(device) then
+    table.insert(attributes, ElectricalMeasurement.attributes.ActivePower)
+    table.insert(attributes, SimpleMetering.attributes.CurrentSummationDelivered)
+  end
+  for _, attribute in pairs(attributes) do
+    device:send(attribute:read(device))
+  end
+end
+
+local do_configure = function(self, device)
+  device:send(device_management.build_bind_request(device, Thermostat.ID, self.environment_info.hub_zigbee_eui))
+  device:send(device_management.build_bind_request(device, PowerConfiguration.ID, self.environment_info.hub_zigbee_eui))
+  device:send(PowerConfiguration.attributes.BatteryVoltage:configure_reporting(device, 30, 21600, 1))
+
+  if is_w500(device) then
+    device:send(device_management.build_bind_request(device, ElectricalMeasurement.ID, self.environment_info.hub_zigbee_eui))
+    device:send(device_management.build_bind_request(device, SimpleMetering.ID, self.environment_info.hub_zigbee_eui))
+  end
+end
+
+local device_added = function(self, device)
+  if is_w500(device) then
+    device:set_field(constants.ELECTRICAL_MEASUREMENT_DIVISOR_KEY, 10, { persists = true })
+    device:set_field(constants.SIMPLE_METERING_DIVISOR_KEY, 1000, { persists = true })
+  end
+
+  device:send(TemperatureMeasurement.attributes.MinMeasuredValue:read(device))
+  device:send(TemperatureMeasurement.attributes.MaxMeasuredValue:read(device))
+  do_refresh(self, device)
+end
+
+local zigbee_thermostat_driver = {
+  supported_capabilities = {
+    Temperature,
+    ThermostatCoolingSetpoint,
+    ThermostatHeatingSetpoint,
+    ThermostatMode,
+    ThermostatOperatingState,
+    RelativeHumidity,
+    PowerMeter,
+    EnergyMeter,
+    Battery,
+    PowerSource,
+    Refresh
+  },
+  zigbee_handlers = {
+    attr = {
+      [PowerConfiguration.ID] = {
+        [PowerConfiguration.attributes.BatteryVoltage.ID] = battery_voltage_handler,
+        [PowerConfiguration.attributes.BatteryAlarmState.ID] = power_source_handler
+      },
+      [xiaomi_utils.OppleCluster] = {
+        [0x00F7] = xiaomi_utils.handler
+      },
+      [Thermostat.ID] = {
+        [Thermostat.attributes.ControlSequenceOfOperation.ID] = supported_thermostat_modes_handler,
+        [Thermostat.attributes.ThermostatRunningState.ID] = thermostat_operating_state_handler,
+        [Thermostat.attributes.ThermostatRunningMode.ID] = thermostat_mode_handler,
+        [Thermostat.attributes.SystemMode.ID] = thermostat_mode_handler,
+        [Thermostat.attributes.MinHeatSetpointLimit.ID] = setpoint_limit_handler_factory('min', 'heat'),
+        [Thermostat.attributes.MaxHeatSetpointLimit.ID] = setpoint_limit_handler_factory('max', 'heat'),
+        [Thermostat.attributes.MinCoolSetpointLimit.ID] = setpoint_limit_handler_factory('min', 'cool'),
+        [Thermostat.attributes.MaxCoolSetpointLimit.ID] = setpoint_limit_handler_factory('max', 'cool'),
+      },
+      [ElectricalMeasurement.ID] = {
+        [ElectricalMeasurement.attributes.ActivePower.ID] = active_power_handler
+      },
+      [SimpleMetering.ID] = {
+        [SimpleMetering.attributes.CurrentSummationDelivered.ID] = current_summation_delivered_handler
+      },
+      [TemperatureMeasurement.ID] = {
+        [TemperatureMeasurement.attributes.MinMeasuredValue.ID] = temperature_measurement_min_max_attr_handler(temperature_measurement_defaults.MIN_TEMP),
+        [TemperatureMeasurement.attributes.MaxMeasuredValue.ID] = temperature_measurement_min_max_attr_handler(temperature_measurement_defaults.MAX_TEMP),
+      }
+    }
+  },
+  capability_handlers = {
+    [ThermostatMode.ID] = {
+      [ThermostatMode.commands.setThermostatMode.NAME] = set_thermostat_mode,
+      [ThermostatMode.commands.auto.NAME] = thermostat_mode_setter(ThermostatMode.thermostatMode.auto.NAME),
+      [ThermostatMode.commands.off.NAME] = thermostat_mode_setter(ThermostatMode.thermostatMode.off.NAME),
+      [ThermostatMode.commands.cool.NAME] = thermostat_mode_setter(ThermostatMode.thermostatMode.cool.NAME),
+      [ThermostatMode.commands.heat.NAME] = thermostat_mode_setter(ThermostatMode.thermostatMode.heat.NAME),
+      [ThermostatMode.commands.emergencyHeat.NAME] = thermostat_mode_setter(ThermostatMode.thermostatMode.emergency_heat.NAME)
+    },
+    [capabilities.refresh.ID] = {
+      [capabilities.refresh.commands.refresh.NAME] = do_refresh,
+    },
+    [capabilities.thermostatCoolingSetpoint.ID] = {
+      [capabilities.thermostatCoolingSetpoint.commands.setCoolingSetpoint.NAME] = set_setpoint_factory(clusters.Thermostat.attributes.OccupiedCoolingSetpoint)
+    },
+    [capabilities.thermostatHeatingSetpoint.ID] = {
+      [capabilities.thermostatHeatingSetpoint.commands.setHeatingSetpoint.NAME] = set_setpoint_factory(clusters.Thermostat.attributes.OccupiedHeatingSetpoint)
+    }
+  },
+  lifecycle_handlers = {
+    doConfigure = do_configure,
+    added = device_added
+  },
+  
+  health_check = false,
+}
+
+defaults.register_for_default_handlers(zigbee_thermostat_driver, zigbee_thermostat_driver.supported_capabilities)
+local thermostat = ZigbeeDriver("zigbee-thermostat", zigbee_thermostat_driver)
+thermostat:run()
